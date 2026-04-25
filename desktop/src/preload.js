8aca3c6a19f9d00917f98caf61b84d9b9e717d24
George Bradford <georgebradford0@proton.me>
2026-04-24 20:36:18 -0700

    Remove dead code

--- desktop/src/preload.js
+++
@@ -1,9 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('api', {
-  loadWords: () => ipcRenderer.invoke('load-words'),
-  translate: (word) => ipcRenderer.invoke('translate', word),
-  speak: (text) => ipcRenderer.invoke('speak', text),
-  loadProgress: () => ipcRenderer.invoke('load-progress'),
-  saveProgress: (word, data) => ipcRenderer.invoke('save-progress', word, data),
-});
